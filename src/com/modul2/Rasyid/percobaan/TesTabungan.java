51ef68b0b675f7f880564d3a9a5ad639da0dc017
Rasyid_ <Rasyid.dot.id@gmail.com>
2021-10-23 23:47:34 +0700

    Menambahkan file program pada modul 3 - percobaan
    Menambahkan TesTabungan.java pada modul 3 - percobaan

---
+++ src/com/modul2/Rasyid/percobaan/TesTabungan.java
@@ -0,0 +1,14 @@
+package com.Rasyid.modul3.percobaan;
+
+public class TesTabungan {
+    public static void main(String[] args) {
+        System.out.println();
+        Tabungan uangku = new Tabungan(5000);
+        System.out.println("Saldo awal tabungan Anda\t: " + uangku.saldo);
+        uangku.ambilUang(1500);
+
+        System.out.println("Jumlah uang yang diambil\t : 1500");
+        System.out.println("Saldo tabungan Anda sekarang adalah\t: " + uangku.saldo);
+    }
+
+}
